f20500d97a403a5fa391008999ff2e26eb58514d
Hasan Ocal <140838926+hasanocal42@users.noreply.github.com>
2023-10-05 22:21:37 +0200

    Delete USAL4M Algo - R. Rebiha/

--- USAL4M Algo - R. Rebiha/04.10.2023/Untitled-1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Bom Dia!");
-    return 0;
-}
--- USAL4M Algo - R. Rebiha/04.10.2023/Untitled-2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int Age = 31;
-    printf("Je m'appelle Hasan j'ai %d", Age);
-    return 0;
-}
